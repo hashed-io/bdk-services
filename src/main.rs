3d6de1ea435f2af5e153ff2c9c0d7cdf609ecfe8
Sebastian Montero <sebastian.montero@gmail.com>
2022-05-23 14:07:57 -0500

    Developed CORS Fairing and configured rocket to enable it.

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,35 @@ use bdk_services::hbdk::{
     errors::Error, Blockchain, Descriptors, Multisig, SignedTrx, Trx, Wallet,
 };
 use bitcoin::Network;
-use rocket::fairing::AdHoc;
 use rocket::serde::{json::Json, Deserialize};
 use rocket::State;
+use rocket::http::Header;
+use rocket::{Request, Response};
+use rocket::fairing::{AdHoc, Fairing, Info, Kind};
 
 #[derive(Deserialize)]
 struct Config {
     network_url: String,
     network: Network,
+}
+
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
 }
 
 /// Returns a new address for the provided output descriptor
@@ -150,4 +171,5 @@ fn rocket() -> _ {
             ],
         )
         .attach(AdHoc::config::<Config>())
+        .attach(CORS)
 }
